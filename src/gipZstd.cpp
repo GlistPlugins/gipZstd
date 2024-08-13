040f6e3e4b8793ee2c8bb54eb8a14a0aff5b44d5
irrld <irrelevantdev@outlook.com>
2024-08-13 19:32:11 +0300

    Initial commit

---
+++ src/gipZstd.cpp
@@ -0,0 +1,52 @@
+//
+// Created by irrel on 13/08/2024.
+//
+
+#include "gipZstd.h"
+
+#include "zstd.h"
+
+namespace gipZstd {
+
+std::vector<uint8_t> compressString(const std::string& input, int compresssionLevel, bool* outSuccess) {
+	size_t maxdistsize = ZSTD_compressBound(input.size());
+	std::vector<uint8_t> compresseddata(maxdistsize);
+
+	size_t compressedsize = ZSTD_compress(
+		compresseddata.data(),
+		maxdistsize,
+		input.data(),
+		input.size(),
+		compresssionLevel // compression level
+	);
+
+	if (ZSTD_isError(compressedsize)) {
+		if (outSuccess) *outSuccess = false;
+		return {};
+	}
+
+	compresseddata.resize(compressedsize);
+	if (outSuccess) *outSuccess = true;
+	return compresseddata;
+}
+
+std::string decompressString(const std::vector<uint8_t>& compresseddata, size_t originalSize, bool* outSuccess) {
+	std::vector<uint8_t> decompresseddata(originalSize);
+
+	size_t decompressedsize = ZSTD_decompress(
+		decompresseddata.data(),
+		originalSize,
+		compresseddata.data(),
+		compresseddata.size()
+	);
+
+	if (ZSTD_isError(decompressedsize)) {
+		if (outSuccess) *outSuccess = false;
+		return {};
+	}
+
+	if (outSuccess) *outSuccess = true;
+	return {decompresseddata.begin(), decompresseddata.end()};
+}
+
+}
